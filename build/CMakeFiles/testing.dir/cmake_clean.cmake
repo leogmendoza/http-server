29fb18d3ba28ca333c479e3623c1e6880116d601
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-05-19 04:07:53 -0400

    Set up CMake

---
+++ build/CMakeFiles/testing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testing.dir/main.cpp.obj"
+  "CMakeFiles/testing.dir/main.cpp.obj.d"
+  "libtesting.dll.a"
+  "testing.exe"
+  "testing.exe.manifest"
+  "testing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Testing by A$AP Rocky";
+
+    return 0;
+}
