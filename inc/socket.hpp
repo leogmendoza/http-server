fcf4deddfd6ab61841340a0f77d51fbee11b6d71
Leopoldo Mendoza <leopoldomendoza03@gmail.com>
2025-06-09 00:51:57 -0400

    Refactor socket class

---
+++ inc/socket.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <winsock2.h>   // Socket functions and types
+
+class Socket {
+    public:
+        // Uninitialized socket handle
+        Socket();
+
+        // Assign socket parameter to handle
+        explicit Socket(SOCKET s);
+
+        ~Socket();
+
+        // Prevent duplicate socket handles
+        Socket(const Socket&) = delete;
+        Socket& operator=(const Socket&) = delete;
+
+        // Initialize by stealing handle from another socket
+        Socket(Socket&& other) noexcept;
+
+        // Transfer ownership from another socket
+        Socket& operator=(Socket&& other) noexcept;
+
+        SOCKET get() const;
+
+        bool is_valid() const;
+
+    private:
+        SOCKET handle_;
+};
